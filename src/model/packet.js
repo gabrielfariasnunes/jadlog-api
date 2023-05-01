bd9fbee669972f10cf501f8e0481148c5ba13d68
gabrielfariasnunes <gabrielfariasdp@gmail.com>
2023-05-01 00:27:42 -0300

    modified:   src/model/packet.js

--- src/model/packet.js
+++ src/model/packet.js
@@ -1,14 +1,14 @@
 const SPLIT_SEPARATOR = "  ";
 const NOT_DOCUMENT = "Sem documento";
 
-const packet = (columns) => {
-  const dateHours = columns[0].text.split(SPLIT_SEPARATOR);
+const packet = (element) => {
+  const dateHours = element[0].text.split(SPLIT_SEPARATOR);
   const data = dateHours[0].trim();
   const hora = dateHours[1].trim();
-  const origem = columns[1].text;
-  const status = columns[2].text;
-  const destino = columns[3].text;
-  const documento = columns[4].text.trim() || NOT_DOCUMENT;
+  const origem = element[1].text;
+  const status = element[2].text;
+  const destino = element[3].text;
+  const documento = element[4].text.trim() || NOT_DOCUMENT;
   return { data, hora, origem, status, destino, documento };
 };
 
