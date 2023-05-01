2b3ba47f4d587b932631e2579368f7f2b67b41d6
gabrielfariasnunes <gabrielfariasdp@gmail.com>
2023-05-01 00:02:10 -0300

    modified:   bin/www/routes.js

--- bin/www/routes.js
+++ bin/www/routes.js
@@ -2,10 +2,10 @@ const { homePageController, findPacketController } = require("./controllers");
 
 const routes = require("express").Router();
 
-const PARAM_PACKET_CODE = "/rastrear/:cte";
-const PARAM_HOME_PAGE = "/";
+const ROUTER_FIND_PACKET = "/rastrear/:cte";
+const ROUTER_HOME_PAGE = "/";
 
-routes.get(PARAM_PACKET_CODE, findPacketController);
-routes.use(PARAM_HOME_PAGE, homePageController);
+routes.get(ROUTER_FIND_PACKET, findPacketController);
+routes.use(ROUTER_HOME_PAGE, homePageController);
 
 module.exports = routes;
