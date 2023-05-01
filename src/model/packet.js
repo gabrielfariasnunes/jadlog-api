1464b3321a3b740af6e09f9e79b9a20f6045f4c5
gabrielfariasnunes <gabrielfariasdp@gmail.com>
2023-04-30 23:50:29 -0300

    Mudanças a serem submetidas: modified:   src/model/packet.js

--- src/model/packet.js
+++ src/model/packet.js
@@ -3,15 +3,13 @@ const MESSAGE_NOT_DOCUMENT = "Sem documento";
 
 const packet = (columns) => {
   const dateHours = columns[0].text.split(SPLIT_SEPARATOR);
-  return {
-    data: dateHours[0].trim(),
-    hora: dateHours[1].trim(),
-    origem: columns[1].text,
-    status: columns[2].text,
-    destino: columns[3].text,
-    documento:
-      columns[4].text.trim() == "" ? MESSAGE_NOT_DOCUMENT : columns[4].text,
-  };
+  const data = dateHours[0].trim();
+  const hora = dateHours[1].trim();
+  const origem = columns[1].text;
+  const status = columns[2].text;
+  const destino = columns[3].text;
+  const documento = columns[4].text.trim();
+  return { data, hora, origem, status, destino, documento };
 };
 
 module.exports = { packet };
